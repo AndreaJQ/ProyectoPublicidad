ed413b38dc7fa9dfa609ef696df02a28207b34f3
leonardomigliorini2 <leonardomigliorini2@gmail.com>
2024-02-22 20:56:49 -0300

    Entidad Tarea

--- src/main/java/PublicityProject/PROYECTOPUBLICIDAD/entity/Tarea.java
+++ src/main/java/PublicityProject/PROYECTOPUBLICIDAD/entity/Tarea.java
@@ -3,8 +3,15 @@ package PublicityProject.PROYECTOPUBLICIDAD.entity;
 
 import PublicityProject.PROYECTOPUBLICIDAD.enumeration.estadoTarea;
 import jakarta.persistence.*;
+import lombok.*;
 import org.hibernate.annotations.GenericGenerator;
-
+@Data
+@Table(name = "usuarios")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 @Entity
 public class Tarea {
     @Id
@@ -14,48 +21,12 @@ public class Tarea {
     private String tarea;
     @Enumerated(EnumType.STRING)
     private estadoTarea estadoTarea;
+    @ManyToOne
     private Proyecto proyecto;
-   /* @ManyToOne
-    private Agente agente;*/
+    @ManyToMany
+    private UserEntity agente;
 
-    public Tarea() {
-    }
 
-    public Tarea(long idTarea, String tarea, PublicityProject.PROYECTOPUBLICIDAD.enumeration.estadoTarea estadoTarea, Proyecto proyecto) {
-        this.idTarea = idTarea;
-        this.tarea = tarea;
-        this.estadoTarea = estadoTarea;
-    }
 
-    public long getIdTarea() {
-        return idTarea;
-    }
 
-    public void setIdTarea(long idTarea) {
-        this.idTarea = idTarea;
-    }
-
-    public String getTarea() {
-        return tarea;
-    }
-
-    public void setTarea(String tarea) {
-        this.tarea = tarea;
-    }
-
-    public PublicityProject.PROYECTOPUBLICIDAD.enumeration.estadoTarea getEstadoTarea() {
-        return estadoTarea;
-    }
-
-    public void setEstadoTarea(PublicityProject.PROYECTOPUBLICIDAD.enumeration.estadoTarea estadoTarea) {
-        this.estadoTarea = estadoTarea;
-    }
-
-    public Proyecto getProyecto() {
-        return proyecto;
-    }
-
-    public void setProyecto(Proyecto proyecto) {
-        this.proyecto = proyecto;
-    }
 }
