2b46cda1aa96bf525e62282254d827e0a0bb2258
AndreaJQ <andreagjalil@gmail.com>
2024-02-23 22:07:54 -0300

    correcciones -db trabajando

--- src/main/java/PublicityProject/PROYECTOPUBLICIDAD/entity/Image.java
+++ src/main/java/PublicityProject/PROYECTOPUBLICIDAD/entity/Image.java
@@ -1,4 +1,20 @@
 package PublicityProject.PROYECTOPUBLICIDAD.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Table(name = "imagen")
+@Entity
 public class Image {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    private String id;
+    private String nombre;
+    private String tipo;
+    @Lob
+    @Basic(fetch = FetchType.LAZY)
+    private byte[] contenido;
 }
