7a8b2fc4e8daf3a9db5b528a56ea7bb95c5a4a6b
edustereo <eduardostereo@hotmail.com>
2024-05-04 13:44:46 -0300

    CRUD Imagen

---
+++ src/main/java/PublicityProject/PROYECTOPUBLICIDAD/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package PublicityProject.PROYECTOPUBLICIDAD.repository;
+
+import PublicityProject.PROYECTOPUBLICIDAD.entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository <Image, String> {
+}
